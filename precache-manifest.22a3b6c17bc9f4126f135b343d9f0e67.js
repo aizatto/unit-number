eaac60a9622ca4a4e174632891c0e83e6816c6c2
Aizat Faiz <aizat.faiz@gmail.com>
2020-02-13 16:44:48 +0800

    Updates

---
+++ precache-manifest.22a3b6c17bc9f4126f135b343d9f0e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312bce4e2d4f5a22d4ce8b3fcc9276fd",
+    "url": "/unit-number/index.html"
+  },
+  {
+    "revision": "efed20c14c3ab32b198b",
+    "url": "/unit-number/static/css/main.c49db850.chunk.css"
+  },
+  {
+    "revision": "89f95761c0de45229610",
+    "url": "/unit-number/static/js/2.260a6c01.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/unit-number/static/js/2.260a6c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efed20c14c3ab32b198b",
+    "url": "/unit-number/static/js/main.eaa97af1.chunk.js"
+  },
+  {
+    "revision": "9e08a32dda0ca12bf815",
+    "url": "/unit-number/static/js/runtime-main.d006dea1.js"
+  }
+]);
